cef6c8f763e8efd7b79393b129619737c33bac17
Hedguhar Domínguez <hedguhar2511@gmail.com>
2023-11-07 17:25:50 -0600

    feat: add features of creation, completion and display of tasks

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 # Create task list
+task_list = []
+
 
 # Add functions for task management
+def add_task(task):
+    task_list.append(task)
+
+
+def complete_task():
+    task_list.pop()
+
+
+def show_tasks():
+    print("Pending tasks:")
+    for i, task in enumerate(task_list):
+        print(f"{i + 1}. {task}")
+
+
+def get_option():
+    option = int(input("Enter option: "))
+    return option
+
+
 def print_menu():
     print("Main menu")
     print("1. Add task")
@@ -8,11 +29,26 @@ def print_menu():
     print("3. Show task list")
     print("4. Exit\n")
 
+
 # Create main function
 def main():
-    print_menu()
+    while True:
+        print_menu()
+        option = get_option()
+        if option == 1:
+            task = input("Write the new task: ")
+            add_task(task)
+        elif option == 2:
+            complete_task()
+        elif option == 3:
+            show_tasks()
+        elif option == 4:
+            break
+        else:
+            print("Invalid option\n")
 
 
 # Call main function
 if __name__ == "__main__":
     main()
+    
