4bda77affdddd628b3d5ba6f69089ddd2f73fce1
Hedguhar Domínguez <hedguhar2511@gmail.com>
2023-11-07 16:40:04 -0600

    feat: add menu

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 # Create task list
 
 # Add functions for task management
+def print_menu():
+    print("Main menu")
+    print("1. Add task")
+    print("2. Complete task")
+    print("3. Show task list")
+    print("4. Exit\n")
 
 # Create main function
+def main():
+    print_menu()
+
 
 # Call main function
+if __name__ == "__main__":
+    main()
